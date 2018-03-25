d96f9beadbb9e7f15cfd4a70128a027838d2b785
Davi Patury <davi.patury@yahoo.com.br>
2018-03-25 12:30:57 -0300

    Improves addlang command

--- commands/addlang.js
+++ commands/addlang.js
@@ -2,56 +2,56 @@
  * O Comando "addlang" adicionará os cargos aos membros.
  */
 
-const languagemanager = require("../utils/languagemanager");
-const langmgr = new languagemanager();
+const LanguageManager = require("../utils/languagemanager");
+const langmgr = new LanguageManager();
+
+module.exports = {
 
 /** Primeiro o metodo run(client, message, args) será executado pelo nosso arquivo message.js
- * Que passara os argumentos atraves do middleware que programamos.
+ * Que passará os argumentos atraves do middleware que programamos.
  */
-exports.run = (client, message, args) => {
+run: (client, message, args) => {
 
-    /** Verificamos se o numero de argumentos é o correto. */
-    if (!(args.length === 1)) return message.reply(`?? Talvez isso possa ajuda-lo: \`\`\`${message.settings.PREFIX}${this.help.usage}\`\`\``);
+    /** Verificamos se o número de argumentos é válido. */
+    if (args.length < 1) return message.reply(`?? Talvez isso possa ajudá-lo: \`\`\`${message.settings.PREFIX}${this.help.usage}\`\`\``);
 
     /** Então verificamos os argumentos e instanciamos o cargo que queremos pelo nome. */
-    let role;
-
     let langs = langmgr.getLanguages();
-    let idx = langs.map(x => x.toLowerCase()).indexOf(args[0].toLowerCase());
+    let role = langs.find(l => l.toLowerCase() === args.join(' ').toLowerCase());
 
-    if (idx != -1) {
-        role = message.guild.roles.find("name", langs[idx]);
-    }
-    else
+    if (!role)
     {
         const emoji = message.guild.emojis.find("name", "thinkkk");
-        message.react((emoji) ? emoji : "🤔");
-        return message.reply(`?? Talvez isso possa ajuda-lo: \`\`\`${message.settings.PREFIX}addlang [${langs.join("|")}]\`\`\``);
+        message.react(emoji || "🤔");
+        return message.reply(`?? Talvez isso possa ajudá-lo: \`\`\`${message.settings.PREFIX}addlang [${langs.join("|")}]\`\`\``);
     }
 
     /** Logo então atribuimos o cargo ao membro e mandamos uma mensagem como resposta
-     * Caso o membro ja possua o cargo então é enviada uma mensagem retornando.
+     * Caso o membro já possua o cargo então é enviada uma mensagem retornando.
      */
-    if (!message.member.roles.exists("name", role.name))
+    if (!message.member.roles.has(role.id))
     {
         message.member.addRole(role);
         return message.reply(`*Beep boop!@* Agora você possui o cargo **${role.name}**`);
     }
     else
     {
-        return message.reply(`Você ja possui este cargo!`);
+        return message.reply(`Você já possui esse cargo!`);
     }
-};
+
+},
 
 /** Aqui podemos colocar mais algumas configurações do comando. */
-exports.conf = {
+conf: {},
+
+/** Aqui exportamos ajuda do comando como o seu nome categoria, descrição, etc... */
+get help () {
+    return {
+        name: "addlang",
+        category: "Moderação",
+        description: "Adiciona um cargo de alguma linguagem de programação a si próprio.",
+        usage: `addlang [${langmgr.getLanguages().join("|")}]`
+    }
+}
 
 };
-
-/** Aqui exportamos ajuda do comando como o seu nome categoria descrição etc... */
-exports.help = {
-    name: "addlang",
-    category: "Moderação",
-    description: "Adiciona um cargo de alguma linguagem de programação a si proprio.",
-    usage: "addlang [java|c|c++|c#|python|kotlin|scala|go|javascript|php|swift|ruby|elixir|rust|assembly]"
-}
