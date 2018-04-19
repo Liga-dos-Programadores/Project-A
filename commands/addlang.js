544c71f4acb6bc7a353128dd3f5e74695eacecdc
Álvaro Filho <alvaro.fduarte@hotmail.com>
2018-04-19 01:16:31 -0300

    Fixado erro que fazia o bot ser crashado.

--- commands/addlang.js
+++ commands/addlang.js
@@ -2,57 +2,69 @@
  * O Comando "addlang" adicionará os cargos aos membros.
  */
 
-const LanguageManager = require("../utils/languagemanager");
+const LanguageManager = require('../utils/languagemanager');
 const langmgr = new LanguageManager();
 
 module.exports = {
+  /** Primeiro o metodo run(client, message, args) será executado pelo nosso arquivo message.js
+   * Que passará os argumentos atraves do middleware que programamos.
+   */
+  run: function(client, message, args) {
+    /** Verificamos se o comando foi usado no privado do bot. */
+    try {
+      /** Verificamos se o número de argumentos é válido. */
+      if (args.length < 1)
+        return message.reply(
+          `?? Talvez isso possa ajudá-lo: \`\`\`${message.settings.PREFIX}${
+            this.help.usage
+          }\`\`\``
+        );
+      /** Então verificamos os argumentos e instanciamos o cargo que queremos pelo nome. */
+      let langs = langmgr.getLanguages();
+      let langName = langs
+        .map(l => l.toLowerCase())
+        .find(l => l === args.join(' ').toLowerCase());
+      let role =
+        langName &&
+        message.guild.roles.find(r => r.name.toLowerCase() === langName);
 
-/** Primeiro o metodo run(client, message, args) será executado pelo nosso arquivo message.js
- * Que passará os argumentos atraves do middleware que programamos.
- */
-run: function(client, message, args) {
+      if (!role) {
+        const emoji = message.guild.emojis.find('name', 'thinkkk');
+        message.react(emoji || '🤔');
+        return message.reply(
+          `?? Talvez isso possa ajudá-lo: \`\`\`${
+            message.settings.PREFIX
+          }addlang [${langs.join('|')}]\`\`\``
+        );
+      }
 
-    /** Verificamos se o número de argumentos é válido. */
-    if (args.length < 1) return message.reply(`?? Talvez isso possa ajudá-lo: \`\`\`${message.settings.PREFIX}${this.help.usage}\`\`\``);
-
-    /** Então verificamos os argumentos e instanciamos o cargo que queremos pelo nome. */
-    let langs = langmgr.getLanguages();
-    let langName = langs.map(l => l.toLowerCase()).find(l => l === args.join(' ').toLowerCase());
-    let role = langName && message.guild.roles.find(r => r.name.toLowerCase() === langName);
-
-    if (!role)
-    {
-        const emoji = message.guild.emojis.find("name", "thinkkk");
-        message.react(emoji || "🤔");
-        return message.reply(`?? Talvez isso possa ajudá-lo: \`\`\`${message.settings.PREFIX}addlang [${langs.join("|")}]\`\`\``);
-    }
-
-    /** Logo então atribuimos o cargo ao membro e mandamos uma mensagem como resposta
-     * Caso o membro já possua o cargo então é enviada uma mensagem retornando.
-     */
-    if (!message.member.roles.has(role.id))
-    {
+      /** Logo então atribuimos o cargo ao membro e mandamos uma mensagem como resposta
+       * Caso o membro já possua o cargo então é enviada uma mensagem retornando.
+       */
+      if (!message.member.roles.has(role.id)) {
         message.member.addRole(role);
-        return message.reply(`*Beep boop!@* Agora você possui o cargo **${role.name}**`);
-    }
-    else
-    {
+        return message.reply(
+          `*Beep boop!@* Agora você possui o cargo **${role.name}**`
+        );
+      } else {
         return message.reply(`Você já possui esse cargo!`);
+      }
+    } catch (err) {
+      message.reply(`Utilize esse comando no server!!`);
     }
+  },
 
-},
+  /** Aqui podemos colocar mais algumas configurações do comando. */
+  conf: {},
 
-/** Aqui podemos colocar mais algumas configurações do comando. */
-conf: {},
-
-/** Aqui exportamos ajuda do comando como o seu nome categoria, descrição, etc... */
-get help () {
+  /** Aqui exportamos ajuda do comando como o seu nome categoria, descrição, etc... */
+  get help() {
     return {
-        name: "addlang",
-        category: "Moderação",
-        description: "Adiciona um cargo de alguma linguagem de programação a si próprio.",
-        usage: `addlang [${langmgr.getLanguages().join("|")}]`
-    }
-}
-
+      name: 'addlang',
+      category: 'Moderação',
+      description:
+        'Adiciona um cargo de alguma linguagem de programação a si próprio.',
+      usage: `addlang [${langmgr.getLanguages().join('|')}]`
+    };
+  }
 };
