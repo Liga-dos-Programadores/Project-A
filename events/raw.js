393b400a1d63b72d97043b342031038599aa3cbf
beescoitu <rafael@beecoding.net>
2019-03-03 20:48:21 -0300

    fix a lil bug

--- events/raw.js
+++ events/raw.js
@@ -6,7 +6,7 @@ module.exports = async (client, event) => {
   if (event.t === 'MESSAGE_REACTION_ADD' || event.t === 'MESSAGE_REACTION_REMOVE') {
     const emojiToRole = require('../emojiRole.json')
     if (emojiToRole.id !== event.d.message_id) return
-    if (!emojiToRole.includes(event.d.emoji.id)) return
+    if (!emojiToRole[event.d.emoji.id]) return
     const channel = client.channels.get(event.d.channel_id)
     const member = channel.guild.members.get(event.d.user_id)
     if (event.t === 'MESSAGE_REACTION_ADD') return member.addRole(channel.guild.roles.get(emojiToRole[event.d.emoji.id]))
