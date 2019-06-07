ad9da04e2937789b21ec481caacf51f4d4f32c0c
Rafael <rafael@beecoding.net>
2019-06-06 22:22:05 -0300

    Oopsie, fixing howtoask

--- commands/howtoask.js
+++ commands/howtoask.js
@@ -11,12 +11,12 @@ module.exports = {
     // Criando embed que sera enviado para o usuário
     let embed = {
       title: 'Como Perguntar',
-      description: '1. Descreva seu problema de forma simples e objetiva.\n2. Sempre procure mandar um exemplo mínimo e reprodutível do problema.\n3. Sempre que compartilhar código, use as tags de Markdown apropriadas.\n\n**Leia mais:** [How to ask](https://stackoverflow.com/help/how-to-ask) e [Markdown](https://gist.github.com/Almeeida/41a664d8d5f3a8855591c2f1e0e07b19#code-blocks)',
+      description: '1. Descreva seu problema de forma simples e objetiva.\n2. Sempre procure mandar um exemplo mínimo e reprodutível do problema.\n3. Sempre que compartilhar código, use as tags de Markdown apropriadas.\n\n**Links:** [How to ask](https://stackoverflow.com/help/how-to-ask) e [Markdown](https://gist.github.com/Almeeida/41a664d8d5f3a8855591c2f1e0e07b19#code-blocks)',
       color: 0xB1103C
     }
 
     // Aqui será enviado o embed no canal que o usuário executo o comando
-    message.channel.send(embed)
+    message.channel.send({ embed })
   },
   /**
     * Aqui podemos colocar mais algumas configurações do comando.
