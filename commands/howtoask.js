409d72d3de3d03236aa7f5f5df748d9d3a8becd1
Rafael <rafael@beecoding.net>
2019-06-06 22:18:13 -0300

    Update howtoask.js

--- commands/howtoask.js
+++ commands/howtoask.js
@@ -11,7 +11,7 @@ module.exports = {
     // Criando embed que sera enviado para o usuário
     let embed = {
       title: 'Como Perguntar',
-      description: '1. Descreva seu problema de forma simples e objetiva.\n2. Sempre procure mandar um exemplo mínimo e reprodutível do problema.\n3. Sempre que compartilhar código, use as tags de Markdown apropriadas.\n\n**Links:** [How to ask](https://stackoverflow.com/help/how-to-ask) e [Markdown](https://gist.github.com/Almeeida/41a664d8d5f3a8855591c2f1e0e07b19#code-blocks)',
+      description: '1. Descreva seu problema de forma simples e objetiva.\n2. Sempre procure mandar um exemplo mínimo e reprodutível do problema.\n3. Sempre que compartilhar código, use as tags de Markdown apropriadas.\n\n**Leia mais:** [How to ask](https://stackoverflow.com/help/how-to-ask) e [Markdown](https://gist.github.com/Almeeida/41a664d8d5f3a8855591c2f1e0e07b19#code-blocks)',
       color: 0xB1103C
     }
 
