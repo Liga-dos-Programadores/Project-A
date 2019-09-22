b6f2e2ca4b35712b1575c888918ce8ed83d7f20d
Álvaro Filho <alvaroddt01@gmail.com>
2019-09-22 00:56:35 -0300

    A new command created 'search'

--- commands/search.js
+++ commands/search.js
@@ -1,15 +1,15 @@
 /**
-* O Comando "search" vai retornar ao usuário um pesquisa feita no duckduckgo.
-*/
+ * O Comando "search" vai retornar ao usuário um pesquisa feita no duckduckgo.
+ */
 const DuckDuckScrape = require("duck-duck-scrape");
 const ddg = new DuckDuckScrape();
 
 module.exports = {
     /**
-    * Primeiro o metodo run(client, message, args) será executado pelo nosso arquivo message.js
-    * Que passará os argumentos atraves do middleware que programamos.
-    */
-    run: function (client, message, args) {
+     * Primeiro o metodo run(client, message, args) será executado pelo nosso arquivo message.js
+     * Que passará os argumentos atraves do middleware que programamos.
+     */
+    run: function(client, message, args) {
         if (args.length < 1) return message.reply(`?? Talvez isso possa ajudá-lo: \`\`\`${process.env.PREFIX}${module.exports.help.usage}\`\`\``)
 
         let result = ddg.search(args.join(" "), 1, "pt-br");
@@ -26,13 +26,13 @@ module.exports = {
         })
     },
     /**
-    * Aqui podemos colocar mais algumas configurações do comando.
-    */
+     * Aqui podemos colocar mais algumas configurações do comando.
+     */
     conf: {},
 
     /**
-    * Aqui exportamos ajuda do comando como o seu nome categoria, descrição, etc...
-    */
+     * Aqui exportamos ajuda do comando como o seu nome categoria, descrição, etc...
+     */
     get help() {
         return {
             name: 'search',
