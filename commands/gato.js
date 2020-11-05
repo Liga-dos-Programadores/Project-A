6acd04f2d2164cc2865e2a8a8d7d9cc346840ad3
luigi <36678917+oluigipo@users.noreply.github.com>
2020-11-05 12:11:12 -0300

    Comando de gato!
    Um comando que faz uma request para uma API que te dá uma URL para uma imagem de gato aleatória.

---
+++ commands/gato.js
@@ -0,0 +1,59 @@
+const Discord = require('discord.js')
+const https = require('https')
+
+const api = 'https://aws.random.cat/meow'
+const titles = [
+  'Lindo gatinho', 'Meow!', '🐱'
+]
+
+function randomTitle() {
+  if (titles.length === 0)
+    return undefined
+  
+  const index = Math.floor(Math.random() * titles.length)
+  return titles[index]
+}
+
+module.exports = {
+
+  run: async (client, message, args) => {
+    
+    http.get(api, {}, (res) => {
+      let data = '';
+      
+      // If it wasn't successful
+      if (res.statusCode != '200') {
+        message.reply('Infelizmente eu não consegui pegar uma foto de gato para você. 😔')
+        return
+      }
+      
+      res.on('data', (chunk) => {
+        data += chunk
+      })
+      
+      res.on('end', () => {
+        let response = JSON.parse(data)
+        
+        const embed = new Discord.RichEmbed()
+          .setTitle(randomTitle())
+          .setImage(response.file)
+          .setColor(message.member ? message.member.displayColor : global.CLIENT_DEFAULT_COLOR)
+
+        message.channel.send(embed)
+      })
+    }).on('error', (error) => console.log(error))
+    
+    
+  },
+
+  conf: {},
+
+  get help () {
+    return {
+      name: 'gato',
+      description: 'Envia um gif ou uma imagem aleatória de um ou mais gatos! API: ' + api,
+      usage: 'gato',
+      category: 'Imagem'
+    }
+  }
+}
