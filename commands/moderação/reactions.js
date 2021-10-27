70d2ae899dce066922ff3d3556b94705ec1fc493
ImGabe <gabrielpmonte@hotmail.com>
2021-10-27 17:07:23 -0300

    fix message reactions

--- commands/moderação/reactions.js
+++ commands/moderação/reactions.js
@@ -3,28 +3,23 @@ const Discord = require('discord.js')
 module.exports = {
   run: async (client, message, args) => {
     if (!message.member.hasPermission(['MANAGE_MESSAGES', 'ADMINISTRATOR'])) { return message.channel.send('> **Você não tem permissão para usar esse comando!**').then(m => m.delete({ timeout: 2000 })) }
-    
-    // const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]})
-    
-    /** Channel */
-    const rolesChannel = message.guild.channels.cache.find((channel) => channel.id === process.env.CANAL_CARGOS)
 
     /** Emojis */
-    const memberEmoji = message.guild.emojis.cache.get(process.env.SERVER_EMOJI)
-    const frontendEmoji = message.guild.emojis.cache.get(process.env.FRONTEND_EMOJI)
-    const backendEmoji = message.guild.emojis.cache.get(process.env.BACKEND_EMOJI)
-    const fullstackEmoji = message.guild.emojis.cache.get(process.env.FULLSTACK_EMOJI)
-    
+    const memberEmoji = process.env.SERVER_EMOJI
+    const frontendEmoji = process.env.FRONTEND_EMOJI
+    const backendEmoji = process.env.BACKEND_EMOJI
+    const fullstackEmoji = process.env.FULLSTACK_EMOJI
+
     /** Roles */
-    const memberRole = process.env.CARGO_MEMBRO
-    const frontendRole = process.env.CARGO_FRONTEND
-    const backendRole = process.env.CARGO_BACKEND
-    const fullstackRole = process.env.CARGO_FULLSTACK
+    const memberRole = message.guild.roles.cache.get(process.env.CARGO_MEMBRO)
+    const frontendRole = message.guild.roles.cache.get(process.env.CARGO_FRONTEND)
+    const backendRole = message.guild.roles.cache.get(process.env.CARGO_BACKEND)
+    const fullstackRole = message.guild.roles.cache.get(process.env.CARGO_FULLSTACK)
 
     const embed = new Discord.MessageEmbed()
       .setColor(process.env.COLOR)
       .setAuthor('Cargos!')
-      .setDescription(`Reaja para obter os cargos! Obs: O cargo **Membro** é o mais importante!\n\n${memberEmoji} - **Cargo @${memberRole}**\n\n${frontendEmoji} - **Cargo Front-End**\n\n${backendEmoji} - **Cargo Back-End**\n\n${fullstackEmoji} - **Cargo Full-Stack**\n\n`)
+      .setDescription(`Reaja para obter os cargos! Obs: O cargo **Membro** é o mais importante!\n\n${memberEmoji} - **Cargo ${memberRole}**\n\n${frontendEmoji} - **Cargo ${frontendRole}**\n\n${backendEmoji} - **Cargo ${backendRole}**\n\n${fullstackEmoji} - **Cargo ${fullstackRole}**\n\n`)
 
     const msg = await message.channel.send(embed)
     msg.react(`${memberEmoji}`)
@@ -32,33 +27,60 @@ module.exports = {
     msg.react(`${backendEmoji}`)
     msg.react(`${fullstackEmoji}`)
 
-    client.on("messageReactionAdd", (reaction, user) => {
+    client.on('messageReactionAdd', async (reaction, user) => {
       if (reaction.message.partial) await reaction.message.fetch()
       if (reaction.partial) await reaction.fetch()
-      if (user.client) return
-      if (!reaction.message.guild) return
+      if (user.bot || !reaction.message.guild) return
+      if (reaction.message.id !== msg.id) return
 
-      if (reaction.message.id == rolesChannel) {
-        if (reaction.emoji.id == memberRole) {
-          await reaction.message.guild.members.cache.get(user.id).roles.add(memberRole)
-        }
-      } else {
-        return
+      switch (reaction.emoji.name) {
+      case memberEmoji:
+        await reaction.message.guild.members.cache.get(user.id).roles.add(memberRole).catch(console.error)
+        break
+
+      case frontendEmoji:
+        await reaction.message.guild.members.cache.get(user.id).roles.add(frontendRole).catch(console.error)
+        break
+
+      case backendEmoji:
+        await reaction.message.guild.members.cache.get(user.id).roles.add(backendRole).catch(console.error)
+        break
+
+      case fullstackEmoji:
+        await reaction.message.guild.members.cache.get(user.id).roles.add(fullstackRole).catch(console.error)
+        break
+
+      default:
+        break
       }
     })
 
-    client.on("messageReactionRemove", (reaction, user) => {
+    client.on('messageReactionRemove', async (reaction, user) => {
       if (reaction.message.partial) await reaction.message.fetch()
       if (reaction.partial) await reaction.fetch()
-      if (user.client) return
+      if (user.bot) return
       if (!reaction.message.guild) return
+      if (reaction.message.id !== msg.id) return
 
-      if (reaction.message.id == rolesChannel) {
-        if (reaction.emoji.id == memberRole) {
-          await reaction.message.guild.members.cache.get(user.id).roles.remove(memberRole)
-        }
-      } else {
-        return
+      switch (reaction.emoji.name) {
+      case memberEmoji:
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(memberRole).catch(console.error)
+        break
+
+      case frontendEmoji:
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(frontendRole).catch(console.error)
+        break
+
+      case backendEmoji:
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(backendRole).catch(console.error)
+        break
+
+      case fullstackEmoji:
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(fullstackRole).catch(console.error)
+        break
+
+      default:
+        break
       }
     })
   },
